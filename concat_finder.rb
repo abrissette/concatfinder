fe5f0dd4cd20b2fbfe986bdd9dc5a7c3deffd52b
Andre Brissette <andre.brissette@jestadigital.com>
2012-10-20 22:10:01 -0400

    add exception raise when no subwords

--- concat_finder.rb
+++ concat_finder.rb
@@ -1,23 +1,21 @@
 require 'set'
 
 class ConcatFinder
-    attr_reader :sub_words_list
+    attr_reader :sub_words_set
     attr_reader :word_candidates_list
 
     def initialize(io)
-      @sub_words_list = []
-      @word_candidates_list = []
+      @sub_words_set = Set.new
+      @word_candidates_list = Array.new
 
       io.each_line do |line |
         line.strip!
-        @sub_words_list << line  if  line.size < 6
+        @sub_words_set << line  if  line.size < 6
         @word_candidates_list << line  if line.size == 6
       end
-      @sub_words_set = @sub_words_list.to_set
 
-      if @word_candidates_list.empty? then
-        raise ArgumentError.new("No valid word candidate in list provided")
-      end
+      raise ArgumentError.new("No valid word candidate")  if @word_candidates_list.empty?
+      raise ArgumentError.new("No valid subwords")  if @sub_words_set.empty?
     end
 
     def find
@@ -33,9 +31,13 @@ class ConcatFinder
   private
     def find_concats(word)
 
-      @sub_words_list.each do | sub_word |
+      @sub_words_set.each do | sub_word |
         if (word.include?(sub_word)) and (@sub_words_set.member?(word.sub(sub_word,""))) then
-          return Array.new([sub_word,(word.sub(sub_word,""))])
+          if word.index(sub_word) == 0 then
+            return Array.new([sub_word,(word.sub(sub_word,""))])
+          else
+            return Array.new([(word.sub(sub_word,"")),sub_word])
+          end
         end
       end
         return nil
--- test/concat_finder_test.rb
+++ test/concat_finder_test.rb
@@ -8,6 +8,7 @@ require "../concat_finder"
 #   - inforce candidate profile on index creation - DONE
 #   - loop to get the words list from command line - DONE
 #   - factor matcher with method/bloc like matches_for_six_letters_concatenated
+#   - Identifiy & extract generic matcher interface of abstract (Extensibility))
 #   - error handling (cant open file, no candidate, etc...)
 #        no candidate - DONE
 
@@ -17,7 +18,7 @@ class ConcatFinderTest < Test::Unit::TestCase
     word_list = StringIO.new("al\nbums\nalbums\npot\npantoufle")
     concat_finder = ConcatFinder.new(word_list)
 
-    assert_equal(['al', 'bums','pot'], concat_finder.sub_words_list)
+    assert_equal(['al', 'bums','pot'].to_set, concat_finder.sub_words_set)
   end
 
   def test_restrict_candidate_to_six_letters_words
@@ -29,6 +30,15 @@ class ConcatFinderTest < Test::Unit::TestCase
 
   def test_throw_an_error_when_zero_candidate
     word_list = StringIO.new("alli\nal\npant\npantoufle")
+
+    assert_raise(ArgumentError) do
+      concat_finder = ConcatFinder.new(word_list)
+    end
+
+  end
+
+  def test_throw_an_error_when_no_subwords
+    word_list = StringIO.new("pantin\nalbums")
 
     assert_raise(ArgumentError) do
       concat_finder = ConcatFinder.new(word_list)
