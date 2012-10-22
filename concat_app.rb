7d004620a18443ea409b67182a79dc6de48ae8cb
Andre Brissette <andre.brissette@jestadigital.com>
2012-10-21 22:40:23 -0400

    add switch -s to create statistical report

--- concat_app.rb
+++ concat_app.rb
@@ -1,25 +1,40 @@
 require 'concat_finder'
 
+  program_start = Time.now
   if (ARGV.size == 0) or (!File.exists?(ARGV[0])) then
     abort("please provide a valid file name")
   end
 
   filename = ARGV[0]
+  report = String.new
+
   File.open(filename,"r") do | file |
 
-    puts "loading words from file " + filename +  "...\n"
-    debut = Time.now
+    report += "******* STATS **********\n"
+    report += "loading words from file " + filename +  "...\n"
+    time_stamp_start = Time.now
     concat_finder = ConcatFinder.new(file)
-    fin = Time.now
-    puts "parsing completed in #{(fin - debut)*1000} millisecondes\n"
-    puts "#{concat_finder.word_candidates_list.size} word candidates\n"
-    puts "#{concat_finder.sub_words_set.size} possible subwords\n"
+    time_stamp_stop = Time.now
+    report += "parsing completed in #{(time_stamp_stop - time_stamp_start)*1000} millisecondes\n"
+    report += "#{concat_finder.word_candidates_list.size} word candidates\n"
+    report += "#{concat_finder.sub_words_set.size} possible subwords\n"
 
-    puts "searching concats...\n"
-    debut = Time.now
+    report += "searching concats...\n"
+    time_stamp_start = Time.now
     result = concat_finder.find
-    fin = Time.now
-    puts "search completed in #{(fin - debut)*1000} millisecondes\n"
-    puts "#{result.size} words are concatenated of 2 smaller ones"
+    time_stamp_stop = Time.now
+    report +=  "search completed in #{(time_stamp_stop - time_stamp_start)*1000} millisecondes\n"
+    report +=  "#{result.size} words are concatenated of 2 smaller ones\n"
 
+    program_stop = Time.now
+    report +=  "Total time for program is #{(program_stop - program_start)*1000} millisecondes\n"
+
+    result.each { | word, concats | puts "#{word}->#{concats.inspect}\n" }
+
+  end
+
+  if ARGV[1] == "-s" then
+    File.open(filename + ".stats", 'w') do |f|
+      f.puts report
+    end
   end
